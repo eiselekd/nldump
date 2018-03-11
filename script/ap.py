bb15f6de298656394021dc9b8886448ae4377331
Konrad Eisele <Konrad Eisele>
2018-03-11 19:03:41 +0000

    all

--- script/ap.py
+++ script/ap.py
@@ -18,19 +18,8 @@ class AP(NL80211):
     def __init__(self, opts, *argv, **kwarg):
         self.opts = opts;
         
-        # get specific groups kwarg
-        if 'groups' in kwarg:
-            groups = kwarg['groups']
-            del kwarg['groups']
-        else:
-            groups = None
-
-        # get specific async kwarg
-        if 'async' in kwarg:
-            async = kwarg['async']
-            del kwarg['async']
-        else:
-            async = False
+        groups = None
+        async = False
 
         # align groups with async
         if groups is None:
@@ -38,37 +27,39 @@ class AP(NL80211):
 
         # continue with init
         super(AP, self).__init__(*argv, **kwarg)
-
-        # do automatic bind
-        # FIXME: unfortunately we can not omit it here
         self.bind(groups, async)
 
     def startap(self, ifindex=-1):
         
         if (ifindex == -1):
+            
+            n = None;
             if isinstance(self.opts.iface,str):
                 n = self.opts.iface
-                try:
-                    from pyroute2.iwutil import IW
-                    iw = IW()
-                    for q in iw.get_interfaces_dump():
-                        ifname = q.get_attr('NL80211_ATTR_IFNAME');
-                        phyname = 'phy%i' % int(q.get_attr('NL80211_ATTR_WIPHY'))
-                        if ifname == n or phyname == n:
-                            ifindex = q.get_attr('NL80211_ATTR_IFINDEX')
-                            break
-                finally:
-                    pass
+            elif len(self.opts.args) > 0:
+                n = self.opts.args.pop(0)
+            try:
+                from pyroute2.iwutil import IW
+                iw = IW()
+                for q in iw.get_interfaces_dump():
+                    ifname = q.get_attr('NL80211_ATTR_IFNAME');
+                    phyname = 'phy%i' % int(q.get_attr('NL80211_ATTR_WIPHY'))
+                    if ifname == n or phyname == n:
+                        ifindex = q.get_attr('NL80211_ATTR_IFINDEX')
+                        break
+            finally:
+                pass
+
         if ifindex == -1:
             raise AP.NoIface();
             
-        print("startap")
+        print("Get interfaces")
         msg = nl80211cmd()
         msg['cmd'] = NL80211_CMD_GET_INTERFACE;
         msg['attrs'] = [['NL80211_ATTR_IFINDEX', ifindex]];
         r = self.nlm_request(msg, msg_type=28, msg_flags=NLM_F_REQUEST | NLM_F_ACK);
         pprint(r);
-
+        
         return
     
 
