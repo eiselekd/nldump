b7e84be9f68d3abd63973504dae642eeb6158eb6
Konrad Eisele <eiselekd@gmail.com>
2018-03-13 21:05:43 +0000

    all

--- libnl3-3.2.29/lib/decode.c
+++ libnl3-3.2.29/lib/decode.c
@@ -13,13 +13,13 @@ static int idx = 0;
 void nlmsg_decode_call(char *decode, struct nl_msg *msg, char *dir)
 {
 	int proto = msg->nm_protocol;
-	int len = msg->nm_size;
+	int len = nlmsg_hdr(msg)->nlmsg_len;
 	char b[256], cmd[256];
 	sprintf(b, "/tmp/nl_%08d_%08d_%s", idx, proto, dir);
 	idx++;
 	FILE *f = fopen(b, "w");
 	if (f) {
-		fwrite((char*)msg, 1, len, f);
+		fwrite((char*)nlmsg_hdr(msg), 1, len, f);
 		fclose(f);
 
 		sprintf(cmd, "%s %s", decode, b);
